fdeb59eed937f9dabc6438d4e569f6aec4a05376
Leonardo Santana <leonardo.santana@locaweb.com.br>
2023-01-07 00:12:03 -0300

    add v1 to api versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+    end
+  end
 end
